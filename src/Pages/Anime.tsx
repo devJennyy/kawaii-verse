27febededfd4e74e632e59614af2b717de73fbdf
Jenny Pieloor <jennypieloor.connect@gmail.com>
2024-04-11 13:34:28 +0800

    Updated UI grid for Anime tab

--- src/Pages/Anime.tsx
+++ src/Pages/Anime.tsx
@@ -2,12 +2,14 @@ import { FiGrid } from "react-icons/fi";
 import { IoIosArrowDown } from "react-icons/io";
 import { LuList, LuSettings2 } from "react-icons/lu";
 import AnimeCard from "../components/AnimeCard";
+import { useState } from "react";
 
 const Anime = () => {
+  const [view, setView] = useState("grid");
 
   return (
     // Main Container
-    <div className="w-full h-[110vh] bg-[#09090B] pt-40 px-20 pb-40">
+    <div className="w-full h-[170vh] bg-[#09090B] pt-40 px-20 pb-40">
       {/* Mini Header */}
       <div className="flex flex-col gap-2">
         <div className="flex flex-row justify-between items-center">
@@ -30,11 +32,21 @@ const Anime = () => {
             </button>
 
             <div className="flex flex-row justify-row items-center w-30 h-12 border border-[#27272A] rounded-md px-2">
-              <button className="flex justify-center items-center rounded-sm w-11 h-8 bg-[#1A1A1A]">
+              <button
+                className={`${
+                  view == "grid" ? `bg-[#1A1A1A]` : `bg-transparent`
+                } flex justify-center items-center rounded-sm w-11 h-8`}
+                onClick={() => setView("grid")}
+              >
                 <FiGrid size={19} />
               </button>
 
-              <button className="flex justify-center items-center rounded-sm w-11 h-8">
+              <button
+                className={`${
+                  view == `list` ? `bg-[#1A1A1A]` : `bg-transparent`
+                } flex justify-center items-center rounded-sm w-11 h-8`}
+                onClick={() => setView("list")}
+              >
                 <LuList size={20} />
               </button>
             </div>
@@ -48,26 +60,56 @@ const Anime = () => {
 
       {/* Content */}
       <div className="w-full pt-10">
-        <div className="flex flex-row justify-start items-center gap-6">
-          <p>Filters:</p>
-          <div className="flex flex-row justify-center items-center gap-2">
-            <p className="flex justify-center items-center text-[12px] bg-[#1A1A1A] rounded-md w-24 h-6 cursor-pointer">Subtitled</p>
-            <p className="flex justify-center items-center text-[12px] bg-[#1A1A1A] rounded-md w-24 h-6 cursor-pointer">Series</p>
+        {view == "grid" ? (
+          <div>
+            <div className="flex flex-row justify-start items-center gap-6">
+              <p>Filters:</p>
+              <div className="flex flex-row justify-center items-center gap-2">
+                <p className="flex justify-center items-center text-[12px] bg-[#1A1A1A] rounded-md w-24 h-6 cursor-pointer">
+                  All
+                </p>
+                <p className="flex justify-center items-center text-[12px] bg-[#1A1A1A] rounded-md w-24 h-6 cursor-pointer">
+                  Series
+                </p>
+              </div>
+            </div>
+
+            {/* Anime List */}
+            <div className="flex flex-col gap-12 mt-16">
+              <div className="flex flex-col gap-10">
+                <p className="flex justify-start border-b border-[#151518] w-full pb-4 text-[20px] font-semibold">
+                  Last 24 Hours
+                </p>
+
+                <div className="flex flex-row items-center gap-6 overflow-hidden">
+                  <AnimeCard />
+                  <AnimeCard />
+                  <AnimeCard />
+                  <AnimeCard />
+                  <AnimeCard />
+                  <AnimeCard />
+                </div>
+              </div>
+
+              <div className="flex flex-col gap-10">
+                <p className="flex justify-start border-b border-[#151518] w-full mt-14 pb-4 text-[20px] font-semibold">
+                  This Past Week
+                </p>
+
+                <div className="flex flex-row items-center gap-6 overflow-hidden">
+                  <AnimeCard />
+                  <AnimeCard />
+                  <AnimeCard />
+                  <AnimeCard />
+                  <AnimeCard />
+                  <AnimeCard />
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-
-        <div className="flex justify-start border-b border-[#151518] w-full mt-14 pb-3">
-          <p className="text-[18px] font-semibold">Popular</p>
-        </div>
-
-        <div className="flex flex-row items-center gap-6 mt-16 overflow-hidden">
-          <AnimeCard/>
-          <AnimeCard/>
-          <AnimeCard/>
-          <AnimeCard/>
-          <AnimeCard/>
-          <AnimeCard/>
-        </div>  
+        ) : (
+          <div className="text-[50px]"> WORK IN PROGRESS! </div>
+        )}
       </div>
     </div>
   );
